dbcfb7663f4c3ea5f5419a671ad0b341017fbf54
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2024-02-16 17:18:33 -0300

    feat(cli): add command to show version

--- systema/cli.py
+++ systema/cli.py
@@ -1,11 +1,18 @@
 import typer
 
+from .__version__ import VERSION
+
 app = typer.Typer()
 
 
 @app.command()
 def serve():
     print("Command to start the server...")
+
+
+@app.command()
+def version():
+    print(VERSION)
 
 
 if __name__ == "__main__":
